628c995be90ddbdf1e252ab143655c40a31b65ed
Siram Rama Venkata Raghunadh <siram.rama-venkata-raghunadh@capgemini.com>
2020-06-25 20:54:30 +0530

    nodeJS Assignment1

--- node-express/index.js
+++ node-express/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const http = require('http');
-
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+
 
 const hostname = 'localhost';
 
@@ -9,6 +14,10 @@ const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 
 app.use(express.static(__dirname+'/public'));
 
